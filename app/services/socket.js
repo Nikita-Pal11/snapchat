3e9b91e5e4a91da0dde828b0d5220ce7ad0c1751
Nikita pal <niki844639@gmail.com>
2025-12-18 14:57:20 +0530

    fresh commit

--- app/services/socket.js
+++ app/services/socket.js
@@ -1,7 +1,7 @@
 // services/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io(process.env.NEXT_PUBLIC_URL, {
   autoConnect: false,
 });
 
